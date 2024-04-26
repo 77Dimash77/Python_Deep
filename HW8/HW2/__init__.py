c65402b7cde75f4deb8a74992b892bfa8b906f6b
Dimash <mwasikzano@gmail.com>
2024-04-26 06:41:00 +0500

    HW 8---

--- HW8/HW2/__init__.py
+++ HW8/HW2/__init__.py
@@ -1,3 +1,6 @@
+# Соберите из созданных на уроке и в рамках домашнего задания функций пакет для работы с файлами разных форматов.
+
+
 from .json_operations import read_json, write_json
 from .csv_operations import read_csv, write_csv
 from .pickle_operations import read_pickle, write_pickle
